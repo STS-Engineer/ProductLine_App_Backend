28eabea765d89117409d565b280c74dcb407c6af
STS-Engineer <administration.sts@avocarbon.com>
2025-10-15 10:20:38 +0100

    Update db.js

--- config/db.js
+++ config/db.js
@@ -7,7 +7,10 @@ const pool = new Pool({
     host: 'avo-adb-002.postgres.database.azure.com',
     database: 'RFQ_DATA',
     password: 'St$@0987',
-    port: 5432
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
